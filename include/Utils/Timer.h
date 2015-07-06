e4caca14af370ea3a8f3db3841757e6333833754
wupa9 <wupaz9@gmail.com>
2015-07-06 14:33:15 -0600

    Implemented Timer class

--- include/Utils/Timer.h
+++ include/Utils/Timer.h
@@ -1,14 +1,31 @@
 #ifndef TIMER_H
 #define TIMER_H
 
+#include <SDL2/SDL.h>
 
 class Timer
 {
     public:
         Timer();
-        virtual ~Timer();
+
+        void start();
+        void stop();
+        void pause();
+        void resume();
+
+        Uint32 getTicks();
+
+        bool isStarted() { return started; }
+        bool isPaused()  { return started && paused; }
 
     private:
+
+        Uint32 startTicks;
+
+        Uint32 pauseTicks;
+
+        bool paused;
+        bool started;
 };
 
 #endif // TIMER_H
