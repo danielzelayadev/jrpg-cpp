9aa9025658f286fbdaace90390ee4659b2f7cd86
wupa9 <wupaz9@gmail.com>
2015-07-06 15:10:28 -0600

    Added Animation class

---
+++ include/Graphics/Animation.h
@@ -0,0 +1,14 @@
+#ifndef ANIMATION_H
+#define ANIMATION_H
+
+
+class Animation
+{
+    public:
+        Animation();
+        virtual ~Animation();
+    protected:
+    private:
+};
+
+#endif // ANIMATION_H
