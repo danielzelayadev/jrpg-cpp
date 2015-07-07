075db5762d0b40979a1ea6508f01ae0be3395275
wupa9 <wupaz9@gmail.com>
2015-07-06 18:08:56 -0600

    Implemented Player class

--- include/Entities/Player.h
+++ include/Entities/Player.h
@@ -2,6 +2,12 @@
 #define PLAYER_H
 
 #include "TMX/RectangleMapObject.h"
+#include "Animation.h"
+
+enum WalkDirs
+{
+   WALK_DOWN, WALK_LEFT, WALK_RIGHT, WALK_UP
+};
 
 class Player
 {
@@ -9,8 +15,19 @@ class Player
         Player(RectangleMapObject* obj);
         virtual ~Player();
 
+        void update();
+        void render(SDL_Renderer* renderer);
+
+        void walk(int dir);
+
     private:
         RectangleMapObject* playerObject;
+        SDL_Texture* spritesheet;
+
+        Animation* movements;
+
+        int speed;
+        string spritesheetSrc;
 
         void loadMOProperties();
 };
--- src/Entities/Player.cpp
+++ src/Entities/Player.cpp
@@ -1,11 +1,101 @@
 #include "Player.h"
 
+#include <sstream>
+#include <iostream>
+
+using namespace std;
+
 Player::Player(RectangleMapObject* obj)
 {
     this->playerObject = obj;
+
+    loadMOProperties();
+}
+
+void Player::loadMOProperties()
+{
+   stringstream strm;
+
+   int framesX, framesY, frameW, frameH, animSpeed, animDir;
+
+   strm << playerObject->getProperty("FrameW");
+   strm >> frameW;
+
+   strm.clear();
+
+   strm << playerObject->getProperty("FrameH");
+   strm >> frameH;
+
+   strm.clear();
+
+   strm << playerObject->getProperty("FramesX");
+   strm >> framesX;
+
+   strm.clear();
+
+   strm << playerObject->getProperty("FramesY");
+   strm >> framesY;
+
+   strm.clear();
+
+   strm << playerObject->getProperty("Animation Speed");
+   strm >> animSpeed;
+
+   strm.clear();
+
+   strm << playerObject->getProperty("Animation Direction");
+   strm >> animDir;
+
+   strm.clear();
+
+   movements = new Animation(framesX, framesY, frameW, frameH, animSpeed, animDir);
+
+   strm << playerObject->getProperty("Speed");
+   strm >> speed;
+
+   spritesheetSrc = playerObject->getProperty("Spritesheet");
+}
+
+void Player::update()
+{
+   movements->animate();
+}
+
+void Player::render(SDL_Renderer* renderer)
+{
+   SDL_Rect frame = movements->getCurrentFrame();
+   SDL_Rect rect = {playerObject->x, playerObject->y, playerObject->width, playerObject->height};
+
+   SDL_RenderCopy(renderer, spritesheet, &frame, &rect);
+}
+
+void Player::walk(int dir)
+{
+   switch(dir)
+   {
+      case WALK_DOWN:
+      playerObject->y += speed;
+      break;
+      case WALK_UP:
+      playerObject->y -= speed;
+      break;
+      case WALK_LEFT:
+      playerObject->x -= speed;
+      break;
+      case WALK_RIGHT:
+      playerObject->x += speed;
+      break;
+   }
+
+   if(!movements->isPlaying())
+   {
+      movements->setCurrentFrame(dir, 0);
+      movements->play();
+   }
 }
 
 Player::~Player()
 {
-    //dtor
+   SDL_DestroyTexture(spritesheet);
+   delete movements;
 }
